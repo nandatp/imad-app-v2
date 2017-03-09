c42d950656aabfe2642516ea13ac37c8ea675d5e
nandatp <nandhutp@gmail.com>
2017-03-09 12:56:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,122 +5,118 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
 var articles = {
-
-articleone: {
-    title: 'Article One | Heern Darji',
-    heading: 'Article One',
-    date: 'Feb 16, 2017',
-    content: `  <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
-
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
-
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
-
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
-
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>`
-
+'article-one': {
+title: 'Article one | Krishna',
+heading:'Article one',
+date: 'February-10-2017',
+content: 
+`<p>
+This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+</p>
+<p>
+This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+</p>
+<p>
+This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+</p>
+<p>
+This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+</p>
+`
 },
-articletwo: {
-    title: 'Article Two | Heern Darji',
-    heading: 'Article Two',
-    date: 'Feb 16, 2017',
-    content: `  <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
+'article-two': {
+title: 'Article two | Krishna',
+heading:'Article Two',
+date: 'February-15-2017',
+content:
+`
+<p>
+This is the content for my second article
+</p>
+`
+},
+'article-three': {
+title: 'Article Three | Krishna',
+heading:'Article Three',
+date: 'February-25-2017',
+content:
+`
+<p>
+This is the content for my Third article
+</p>
+`
+}
+};
+function createTemplate (data){
+var date = data.date;
+var content = data.content;
+var heading = data.heading;
+var title = data.title;
 
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
+var htmlTemplate =  `
 
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
+<html>
+<head>
+    <title>
+        ${title}
+        
+        
+    </title>
+    <meta name="viewport" content="width=device-width,intial-scale=1"/>
+     <link href="/ui/style.css" rel="stylesheet" />
+     
+</head>
+<body>
+    <div class="container">
+        
+    <div>
+        <a href="/">Home</a>
+        
+    </div>
+    <hr/>
+    <h3>
+        ${heading}
+        
+    </h3>
+    <div>
+        ${date}
+        
+    </div>
+    <div>
+        
+        ${content}
+    </div>
+    </div>
+</body>
+</html>
 
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
-
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>`
-                  },
-articlethree: {
-     title: 'Article Three | Heern Darji',
-    heading: 'Article Three',
-    date: 'Feb 16, 2017',
-    content: `  <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
-
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
-
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
-
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>
-
-                    <p>This is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.his is the fucking first line.</p>`
+`;
+return htmlTemplate;
 }
 
-};
-
-function createTemplate (data) {
-    var title = data.title;
-    var heading = data.heading;
-    var date = data.date;
-    var content = data.content;
-    var htmlTemplate = `
-        <html>
-          <head>
-                            <title>${title}</title>
-                             <link href="/ui/style.css" rel="stylesheet" />
-                        </head>
-
-
-                        <body>
-                            <div class="container">
-                                <div>
-                                <a href="/">Home</a>
-                                </div>
-                                </hr>
-                                <h3>${heading}</h3>
-                                <div>
-                                    ${date}
-                                </div>
-
-                                <div>
-                                ${content}
-
-                                </div>
-                            </div>
-                        </body>
-                    </html>
-                    `;
-
-
-    return htmlTemplate;
-
-};
-
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/:articleName', function (req,res){
+var articleName = req.params.articleName;
+res.send(createTemplate(articles[articleName]));
 });
-
-/*app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-*/
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var port = 8080; 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
