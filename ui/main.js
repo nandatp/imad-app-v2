f1d113c85331dec1ccfd3b357b81caca9adf21a6
nandatp <nandhutp@gmail.com>
2017-03-10 12:22:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHtml = 'New Value';
 
 //move img
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function () {
     img.style.marginleft = '100px';
     };
