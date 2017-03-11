cd9f564a79e4bbabcb749ce37cfd6d77ccf616b8
nandatp <nandhutp@gmail.com>
2017-03-11 18:15:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,12 @@ var htmlTemplate =  `
 return htmlTemplate;
 }
 
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+    
+});    
+    
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
