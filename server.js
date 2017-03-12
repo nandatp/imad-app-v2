2743adc563195bf8d51e9dc51930f1ed3a7230cf
nandatp <nandhutp@gmail.com>
2017-03-13 01:21:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,9 @@ var express = require('express');
    res.send(createTemplate(articles[articleName]));
  });
  
+  app.get('/ui/main.js', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+ });
   
  app.get('/ui/style.css', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
