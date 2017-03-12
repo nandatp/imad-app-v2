8baecd7efb862de98c0ea3529b8c6c510ee0f00f
nandatp <nandhutp@gmail.com>
2017-03-12 12:09:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,125 +5,143 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-'article-one': {
-title: 'Article one | Krishna',
-heading:'Article one',
-date: 'February-10-2017',
-content: 
-`<p>
-This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-</p>
-<p>
-This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-</p>
-<p>
-This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-</p>
-<p>
-This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-</p>
-`
-},
-'article-two': {
-title: 'Article two | Krishna',
-heading:'Article Two',
-date: 'February-15-2017',
-content:
-`
-<p>
-This is the content for my second article
-</p>
-`
-},
-'article-three': {
-title: 'Article Three | Krishna',
-heading:'Article Three',
-date: 'February-25-2017',
-content:
-`
-<p>
-This is the content for my Third article
-</p>
-`
-}
+
+var articles = { 
+    'article-one': {
+    title:'Article one [jaiprince17]',
+    heading: 'Article one',
+    date: 'feb 8, 2017',
+    content: `
+    <p>
+    tHIS Is the content of the Article one
+    tHIS Is the content of the Article one
+    tHIS Is the content of the Article one
+    </p>
+    <p2>
+    tHIS Is the content of the Article one
+    tHIS Is the content of the Article one
+    tHIS Is the content of the Article one
+    </p2> `
+     },
+    'article-two': { 
+    title:'Article two [jaiprince17]',
+    heading: 'article two',
+    date: 'feb 9, 2017',
+    content:`
+    <p>
+    tHIS Is the content of the Article two
+    tHIS Is the content of the Article two
+    tHIS Is the content of the Article two
+    </p>
+    <p2>
+    tHIS Is the content of the Article two
+    tHIS Is the content of the Article two  
+    tHIS Is the content of the Article two
+    </p2> `
+     },
+    'article-three': { 
+    title:'Article three [jaiprince17]',
+    heading: 'article three',
+    date: 'feb 10, 2017',
+    content:`
+    <p>
+    tHIS Is the content of the Article three
+    tHIS Is the content of the Article three
+    tHIS Is the content of the Article three
+    </p>
+    <p2>
+    tHIS Is the content of the Article three
+    tHIS Is the content of the Article three
+    tHIS Is the content of the Article three
+    </p2> `
+     }
+    
 };
-function createTemplate (data){
-var date = data.date;
-var content = data.content;
-var heading = data.heading;
-var title = data.title;
-
-var htmlTemplate =  `
-
-<html>
-<head>
+   
+function createTemplate (data)  {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate =  ` 
+    <html>
+    <head>
     <title>
-        ${title}
-        
-        
+    ${title}
     </title>
-    <meta name="viewport" content="width=device-width,intial-scale=1"/>
-     <link href="/ui/style.css" rel="stylesheet" />
-     
-</head>
-<body>
+    <meta name="viewport" content="width=device-width, initi-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
     <div class="container">
-        
     <div>
-        <a href="/">Home</a>
-        
+    <a href="/">Home</a>
     </div>
     <hr/>
     <h3>
-        ${heading}
-        
+    ${heading}
     </h3>
     <div>
-        ${date}
-        
+    ${date}
     </div>
-    <div>
-        
-        ${content}
-    </div>
-    </div>
+  ${content}
+</div>
 </body>
 </html>
-
-`;
-return htmlTemplate;
+     `;
+     return htmlTemplate;
 }
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
    counter = counter + 1;
    res.send(counter.toString());
+
     
-});    
-    
-app.get('/', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req,res){
+var names = [] ;
+app.get('/submit-name', function(req, res) { //  /submit-name?name=xxxxx
+    // get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+   // JSON: Javascript Object Notataion 
+    res.send(JSON.stringify(names)); 
+});
+
+app.get('/:articleName', function (req, res) {
+// articleName == article-one
+// articles[articleName] == {} content object for article one
 var articleName = req.params.articleName;
-res.send(createTemplate(articles[articleName]));
+  res.send(createTemplate(articles[articleName]));
+
 });
 
 app.get('/ui/style.css', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 
 app.get('/ui/main.js', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+
 });
+
+
 app.get('/ui/madi.png', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+
+    
 });
 
-var port = 8080; 
 
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
