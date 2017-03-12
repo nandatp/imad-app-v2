2af13694521bafad2cec4ed97f9349ff4c5efeed
nandatp <nandhutp@gmail.com>
2017-03-12 12:09:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,60 @@
+// counter code
 var button = document.getElementById('counter');
-Var counter = 0;
 
 button.onclick = function () {
     
-    // Make a request to the counter endpoint
+    // create a request object
+    var request = new XMLHttpRequest();
     
-    // Capture the response and store it in a variable
+    // capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+        // take some action
+        if (request.status === 200) {
+        var counter = request.responseText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+
+        }
+    }
+    // not done yet
+};
+// make the request
+request.open('GET','http://jaiprince17.imad.hasura-app.io/counter',true);
+request.send(null);
+
+};
+
+// Submit name
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
     
-    // Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter;
+        // create a request object
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+        // take some action
+        if (request.status === 200) {
+             // capture a list of names and render it as a list
+              var names = request.responseText;
+             names = JSON.parse(names);
+    var list = '';
+    for (var i=0; i<names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+      
+        }
+    }
+    // not done yet
+};
+// make the request
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+request.open('GET','http://jaiprince17.imad.hasura-app.io/submit-name?name='+name, true);
+request.send(null);
+    
 };
