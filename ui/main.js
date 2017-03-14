232fc5558838209ae0785ad56bd579b019916239
nandatp <nandhutp@gmail.com>
2017-03-14 22:06:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,16 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = "New Value";
+// counter Code
+var button = document.getElementById("counter");
+var counter = 0;
+button.onclick = function() {
+ 
+ 
+// Make a request to counter end point
 
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-  var interval = setInterval(moveRight, 50);
-  img.style.marginLeft = '100px'; 
-};
+// capture the response
+
+// render the response
+
+counter = counter + 1;
+var span = document.getElementById("count");
+span.innerHTML = counter.toString();
+} ;
