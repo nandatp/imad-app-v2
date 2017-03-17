b2331216c7e5db14716dff701832e803d2a29b02
nandatp <nandhutp@gmail.com>
2017-03-17 12:37:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,25 @@ request.onreadystatechange = function () {
 request.open('GET', 'http://nandatp.imad.hasura-app.io/counter', true);
 request.send(null);
 };
+
+//submit name
+
+var nameinput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  // Make a request to the server and send the name
+  
+  
+  // Capture a list of names and render it as a list
+  var names = ['name1, name2, name3, name4'];
+  var list = '';
+  for(var i=0; i<names.length; i++){
+  list += '<li>' + names[i] + '</li>';
+    }
+ var ul = document.getElementById('namelist');
+ ul.innerHTML = list;
+};
+
+  
+};
