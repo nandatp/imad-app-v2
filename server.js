170ba1940e7034018584581e389bad543d1c5eff
nandatp <nandhutp@gmail.com>
2017-03-20 11:35:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ var express = require('express');
   app.get('/articles/:articleName', function (req, res) {
       // articleName == article-one
       //articles[articleName] = {} content object for article one
-    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
        if (err) {
            res.status(500).send(err.toString());
        } else {
